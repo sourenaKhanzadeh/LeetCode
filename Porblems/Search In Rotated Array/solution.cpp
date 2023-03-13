1988e7c88415876d789babcb8717607e01c1957e
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2023-03-12 22:30:00 -0400

    Search In Rotated Array added

---
+++ Porblems/Search In Rotated Array/solution.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0;
+        int right = n-1;
+        int mid= 0;
+        while(left <= right){
+            mid = left + (right - left) / 2;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[mid] >= nums[left]) {
+                if(target >= nums[left] && target <= nums[mid])
+                {
+                    right = mid - 1;
+                }
+                else left = mid + 1;
+            }
+            else {
+                if(target >= nums[mid] && target <= nums[right])
+                    left = mid + 1;
+                else right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
